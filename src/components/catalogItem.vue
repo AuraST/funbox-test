1893f36dc381e944dcd9f18fa507bbbe2c94e76f
aura.s.t <aura.s.t@yandex.ru>
2020-09-17 12:08:17 +0300

    Сделана начальная структура проекта

---
+++ src/components/catalogItem.vue
@@ -0,0 +1,10 @@
+<template>
+  {{catalogItem}}
+</template>
+
+<script>
+export default {
+  name: 'catalogItem',
+  props: ['catalogItem']
+}
+</script>
---
+++ src/views/catalog.vue
@@ -0,0 +1,26 @@
+<template>
+  <catalogItem :catalogItem="catalogList" />
+</template>
+
+<script>
+  import catalogItem from '@/components/catalogItem'
+
+  export default {
+    name: "catalog",
+    data () {
+        return {
+            catalogList: {}
+        }
+    },
+    mounted() {
+        fetch('../data/catalog.json')
+        .then(response => response.json())
+        .then(json => {
+            this.catalogList = json
+        })
+    },
+    components: {
+        catalogItem
+    }
+  }
+</script>
