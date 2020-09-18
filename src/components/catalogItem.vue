97be8d8892f86984143d035df7615e8a96ac9d0b
aura.s.t <aura.s.t@yandex.ru>
2020-09-18 18:01:03 +0300

    Добавлены доработки по верстке страницы

--- src/components/catalogItem.vue
+++ src/components/catalogItem.vue
@@ -1,21 +1,30 @@
 <template>
   <div :class="['catalog-item', { 'catalog-item--disabled' : catalogItem.active }]">
-    <div class="catalog-item__block">
-      <div class="catalog-item__top">
-        <p class="catalog-item__text">Сказачное заморское яство</p>
-        <p class="catalog-item__title">
-          Нямушка <br>
-          <span class="catalog-item__title--small">с {{ catalogItem.fill }}</span>
-        </p>
-        <p class="catalog-item__info" v-if="catalogItem.countPortion"><span class='text--bold' v-html="catalogItem.countPortion"></span> порций</p>
-        <p class="catalog-item__info" v-if="catalogItem.gift"><span v-html="catalogItem.gift"></span> в подарок</p>
-        <p class="catalog-item__info" v-if="catalogItem.add">{{ catalogItem.add }}</p>
-      </div>
-      <div class="catalog-item__bottom">
-        <div class="catalog-item__param"><span class="catalog-item__param-count">{{ catalogItem.countKg }}</span><br> кг</div>
+    <div class="catalog-item__card">
+      <div class="catalog-item__block">
+        <div class="catalog-item__top">
+          <p class="catalog-item__text">Сказачное заморское яство</p>
+          <p class="catalog-item__title">
+            Нямушка <br>
+            <span class="catalog-item__title--small">с {{ catalogItem.fill }}</span>
+          </p>
+          <p class="catalog-item__info" v-if="catalogItem.countPortion"><span class='text--bold'>{{ catalogItem.countPortion }}</span> порций</p>
+          <p class="catalog-item__info" v-if="catalogItem.gift"><span v-html="catalogItem.gift"></span> в подарок</p>
+          <p class="catalog-item__info" v-if="catalogItem.add">{{ catalogItem.add }}</p>
+        </div>
+        <div class="catalog-item__bottom">
+          <div class="catalog-item__param">
+            <p class="catalog-item__param-count">
+              <span class="catalog-item__param-value">{{ catalogItem.countKg }}</span><br> 
+              кг
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="catalog-item__other" v-html="catalogItem.text"></div>
+    <div class="catalog-item__other">
+      <p class="catalog-item__other-text" v-html="catalogItem.text"></p>
+    </div>
   </div>
 </template>
 
